ee928b638e485006e71b9fe4333badaf0846117b
Nikita <nks.off@gmail.com>
2016-09-19 03:35:00 +0300

    add forgotten httpd.hrl

---
+++ httpd.hrl
@@ -0,0 +1,16 @@
+-type http_method() :: binary().
+-type http_path() :: binary().
+-type http_code() :: 100..600.
+
+-record(request, {
+          method :: http_method(),
+          path :: http_path()
+         }).
+
+-record(response, {
+          code = 400 :: http_code(),
+          body = "" :: binary(),
+          content_type = "text/html" :: binary()
+         }).
+
+-define(DELIMITERS, [<<" ">>, <<"?">>, <<"\r">>, <<"\n">>, <<"\r\n">>]).
